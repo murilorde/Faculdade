b36253ec64fec8f8b3b6d8e952b2016c9ab5a7a1
murilorde <114358274+murilorde@users.noreply.github.com>
2022-09-25 15:25:19 -0300

    Add files via upload

---
+++ Tabuada.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b; 
+    
+    printf("Digite um número: ");
+    scanf ("%d", &a);
+   
+printf("\n===================\n");
+printf("TABUADA DO 1 até %d\n", a);
+printf("===================\n");
+
+
+for (int e = 1;e <= a; e++){
+    for (int i = 1; i <= 10; i++){
+        b = e*i;
+        printf ("%d x %d = %d\n", e, i, b);
+         if(i == 10){
+             printf("\n///////////////\n\n");
+         }
+    }
+}
+   
+   
+    return 0;
+}
